00457185c5bffe086f8957b1b6025b1e7348a25a
Piotr Filipiak <kiddhkane@gmail.com>
2025-05-20 01:26:44 +0200

    Added colors. Changed bg color. Adding fonts.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const outfit = localFont({
+  src: "../public/outfit/Outfit-VariableFont_wght.ttf",
+  variable: "--font-outfit",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const youngSerif = localFont({
+  src: "../public/young-serif/YoungSerif-Regular.ttf",
+  variable: "--font-young-serif",
 });
 
 export const metadata: Metadata = {
@@ -24,10 +24,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={` ${outfit.className} antialiased`}>
+        <main>{children}</main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="">
-      <p>Just starting</p>
-    </div>
+    <section className="flex justify-center items-start min-h-dvh p-24">
+      <div className="bg-white p-4 rounded-2xl">
+        <Image src="/omelette.jpeg" width={1312} height={600} alt="asdf" />
+        <h1 className="text-black text-5xl">Simple Omelette Recipe</h1>
+        <p className="text-stone-100">Just starting</p>
+        <p className="text-stone-900">Just starting</p>
+      </div>
+    </section>
   );
 }
