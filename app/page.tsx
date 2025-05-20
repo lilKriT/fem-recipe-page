7f6140a7ccb5c256e24dccb784a1c6990d4654d7
Piotr Filipiak <kiddhkane@gmail.com>
2025-05-20 15:17:29 +0200

    Styled Instructions

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,9 @@ export default function Home() {
 
         {/* Preparation */}
         <div className="bg-rose-50 p-8 rounded-3xl">
-          <h2 className="text-rose-800 text-2xl font-bold">Preparation time</h2>
+          <h2 className="text-brown-800 text-2xl font-bold">
+            Preparation time
+          </h2>
           <ul className="mt-4 list-disc list-inside marker:text-pink-800 marker:text-base flex flex-col gap-2">
             <li>
               <strong className="pl-4">Total:</strong> Approximately 10 minutes
@@ -67,40 +69,51 @@ export default function Home() {
           </ul>
         </div>
 
+        <hr className="text-stone-600/50" />
+
         {/* Instructions */}
         <div>
-          <h2>Instructions</h2>
-          <ol className="list-decimal list-inside">
-            <li>
-              <strong>Beat the eggs:</strong> In a bowl, beat the eggs with a
-              pinch of salt and pepper until they are well mixed. You can add a
-              tablespoon of water or milk for a fluffier texture.
+          <h2 className="text-3xl font-bold font-outfit text-brown-800">
+            Instructions
+          </h2>
+          <ol className="mt-4 ml-8 list-decimal flex flex-col gap-2 marker:font-bold marker:text-brown-800">
+            <li className="pl-4">
+              <strong className="text-stone-600!">Beat the eggs:</strong> In a
+              bowl, beat the eggs with a pinch of salt and pepper until they are
+              well mixed. You can add a tablespoon of water or milk for a
+              fluffier texture.
             </li>
-            <li>
-              <strong>Heat the pan:</strong> Place a non-stick frying pan over
-              medium heat and add butter or oil.
+            <li className="pl-4">
+              <strong className="text-stone-600!">Heat the pan:</strong> Place a
+              non-stick frying pan over medium heat and add butter or oil.
             </li>
-            <li>
-              <strong>Cook the omelette:</strong> Once the butter is melted and
-              bubbling, pour in the eggs. Tilt the pan to ensure the eggs evenly
-              coat the surface.
+            <li className="pl-4">
+              <strong className="text-stone-600!">Cook the omelette:</strong>{" "}
+              Once the butter is melted and bubbling, pour in the eggs. Tilt the
+              pan to ensure the eggs evenly coat the surface.
             </li>
-            <li>
-              <strong>Add fillings (optional):</strong> When the eggs begin to
-              set at the edges but are still slightly runny in the middle,
-              sprinkle your chosen fillings over one half of the omelette.
+            <li className="pl-4">
+              <strong className="text-stone-600!">
+                Add fillings (optional):
+              </strong>{" "}
+              When the eggs begin to set at the edges but are still slightly
+              runny in the middle, sprinkle your chosen fillings over one half
+              of the omelette.
             </li>
-            <li>
-              <strong>Fold and serve:</strong> As the omelette continues to
-              cook, carefully lift one edge and fold it over the fillings. Let
-              it cook for another minute, then slide it onto a plate.
+            <li className="pl-4">
+              <strong className="text-stone-600!">Fold and serve:</strong> As
+              the omelette continues to cook, carefully lift one edge and fold
+              it over the fillings. Let it cook for another minute, then slide
+              it onto a plate.
             </li>
-            <li>
-              <strong>Enjoy:</strong> Serve hot, with additional salt and pepper
-              if needed.
+            <li className="pl-4">
+              <strong className="text-stone-600!">Enjoy:</strong> Serve hot,
+              with additional salt and pepper if needed.
             </li>
           </ol>
         </div>
+
+        <hr className="text-stone-600/50" />
 
         {/* Nutrition */}
         <div>
